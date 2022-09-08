3bd88f9900d35cbddc8f4b096770a883e9c871ac
arm-out <arminsuraj@gmail.com>
2022-09-08 06:51:15 -0700

    home html and styling

---
+++ components/Text.styles.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+
+export const SectionHeader = styled.h1`
+    font-family: var(--ff-primary);
+    font-size: 45px;
+    font-weight: 500;
+    color: var(--clr-ted-black);
+
+`
+
+export const Sub = styled.h2`
+    font-family: var(--ff-primary);
+    font-size: 27px;
+    font-weight: 500;
+    color: var(--clr-sub-gray);
+
+`
+
+export const Paragraph = styled.p`
+    font-family: var(--ff-primary);
+    font-size: 22px;
+    font-weight: 400;
+    color: var(--clr-ted-black);
+`
+
