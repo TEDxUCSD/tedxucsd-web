0432b26e704ae8cda6541587b57d4b0d09b1461b
arm-out <arminsuraj@gmail.com>
2023-04-01 13:23:01 -0700

    init

--- pages/_app.js
+++
@@ -1,8 +0,0 @@
-import Layout from '../components/Layout';
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
