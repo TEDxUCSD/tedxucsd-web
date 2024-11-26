ef4fb44d80e08844474f899014960f844b71d924
trishatong <trtong1@gmail.com>
2024-11-25 19:19:00 -0800

    2024 events page

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -13,8 +13,8 @@ module.exports = {
 				serif: ["Playfair Display", ...defaultTheme.fontFamily.serif],
 			},
 			backgroundImage: {
-				"expiration-date": "url('/event-img/2024/banner-2024.jpg')",
-				"word-play": "url('/event-img/2023/banner-2023.jpg')",
+				"expiration-date": "url('/event-img/2024/banner-2024.png')",
+				"word-play": "url('/event-img/2023/banner-2023.png')",
 				"sixth-sense": "url('/event-img/2022/banner.jpg')",
 				constellate: "url('/event-img/2021/banner.jpg')",
 				rebooted: "url('/event-img/2020/banner.jpg')",
